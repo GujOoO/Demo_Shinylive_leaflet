e5a6286307b4edb3ece6152508b5d3fa6fc1b039
GujOoO <84066601+GujOoO@users.noreply.github.com>
2024-04-07 19:54:00 +0200

    update v2

--- Rapp/app.R
+++ Rapp/app.R
@@ -1,53 +1,70 @@
-library(sf)
-wbd_reg2 <- st_read("https://raw.githubusercontent.com/khondula/leaflet-in-R-shinydemo2/master/data/wbd_reg2.geojson") 
-wbd_reg2 <- wbd_reg2[wbd_reg2$REG == "02",] %>%
-   st_transform(4326)
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
 
-# user interface with map and color scheme widget
-ui <- fluidPage(
-   leafletOutput("map", height = 800),
-   absolutePanel(top = 10, right = 10, draggable = TRUE,
-      selectInput(inputId = "colors",
-         label = "Color Scheme",
-         choices = row.names(brewer.pal.info))
-   ))
-# server definition
-server <- function(input, output) {
-   output$map <- renderLeaflet({
-      
-      # use this line with leafletproxy for default colors
-      pal <- colorNumeric("BrBG", wbd_reg2$AREA) 
-      
-      # map with polygons and legend
-      leaflet() %>%
-         addTiles() %>%
-         setView(lng = -76.505206, lat = 38.9767231, zoom = 7) %>%
-         addPolygons(data = wbd_reg2, 
-            fill = TRUE,
-            color = "black", 
-            weight =1,
-            fillColor = ~pal(AREA), # color is function of the wbd AREA column
-            fillOpacity = 0.8,
-            popup = ~HUC_NAME) %>%
-         addLegend(position = "bottomright", pal = pal, values = wbd_reg2$AREA)
+ui <- bootstrapPage(
+   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+   leafletOutput("map", width = "100%", height = "100%"),
+   absolutePanel(top = 10, right = 10,
+      sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
+         value = range(quakes$mag), step = 0.1
+      ),
+      selectInput("colors", "Color Scheme",
+         rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+      ),
+      checkboxInput("legend", "Show legend", TRUE)
+   )
+)
+
+server <- function(input, output, session) {
+   
+   # Reactive expression for the data subsetted to what the user selected
+   filteredData <- reactive({
+      quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
    })
    
-   # update map using leaflet proxy instead of recreating the whole thing
-   # use observe to color layer as needed
+   # This reactive expression represents the palette function,
+   # which changes as the user makes selections in UI.
+   colorpal <- reactive({
+      colorNumeric(input$colors, quakes$mag)
+   })
+   
+   output$map <- renderLeaflet({
+      # Use leaflet() here, and only include aspects of the map that
+      # won't need to change dynamically (at least, not unless the
+      # entire map is being torn down and recreated).
+      leaflet(quakes) %>% addTiles() %>%
+         fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+   })
+   
+   # Incremental changes to the map (in this case, replacing the
+   # circles when a new color is chosen) should be performed in
+   # an observer. Each independent set of things that can change
+   # should be managed in its own observer.
    observe({
-      # color palette is defined according to the user's input
-      pal <- colorNumeric(input$colors, wbd_reg2$AREA)
+      pal <- colorpal()
       
-      # the map is updated with the new prefered color palette
-      # notice we use leafletProxy() instead of leaflet()
-      leafletProxy("map") %>%
-         clearShapes() %>% clearControls() %>%
-         addPolygons(data = wbd_reg2, fill = TRUE,
-            color = "black", weight =1,
-            fillColor = ~pal(AREA),
-            fillOpacity = 0.8,
-            popup = ~HUC_NAME) %>%
-         addLegend(position = "bottomright", pal = pal, values = wbd_reg2$AREA)
+      leafletProxy("map", data = filteredData()) %>%
+         clearShapes() %>%
+         addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
+            fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
+         )
+   })
+   
+   # Use a separate observer to recreate the legend as needed.
+   observe({
+      proxy <- leafletProxy("map", data = quakes)
+      
+      # Remove any existing legend, and only if the legend is
+      # enabled, create a new one.
+      proxy %>% clearControls()
+      if (input$legend) {
+         pal <- colorpal()
+         proxy %>% addLegend(position = "bottomright",
+            pal = pal, values = ~mag
+         )
+      }
    })
 }
+
 shinyApp(ui, server)
